3f15c92ae330b29101c6bf19b5c705cb6f273ea1
Oksana Demchenko <sulichka33@gmail.com>
2025-08-05 16:20:47 +0300

    modified index and js files

--- js/task-3.js
+++ js/task-3.js
@@ -4,8 +4,7 @@ function getElementWidth(content, padding, border) {
   const contentWidth = parseFloat(content);
   const paddingWidth = parseFloat(padding);
   const borderWidth = parseFloat(border);
-  const totalWidth = contentWidth + 2 * paddingWidth + 2 * borderWidth;
-  return totalWidth;
+  return contentWidth + 2 * paddingWidth + 2 * borderWidth;
 }
 
 console.log(getElementWidth('50px', '8px', '4px')); // 74
